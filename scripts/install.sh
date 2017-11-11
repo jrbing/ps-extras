f98abbbb1dcc12d4dabf73e98a639857aae5f885
JR Bing <jr@jrbing.com>
2017-11-10 21:12:27 -0500

    Correct GID

--- scripts/install.sh
+++ scripts/install.sh
@@ -52,8 +52,8 @@ function create_buildroot() {
 
 function create_buildroot_user() {
   echoinfo "Creating buildroot user and group"
-  sudo groupadd --gid 1001 builder
-  sudo useradd --system --shell /bin/false --uid 1099 --gid 1000 builder
+  sudo groupadd --gid 1099 builder
+  sudo useradd --system --shell /bin/false --uid 1099 --gid 1099 builder
 }
 
 function modify_buildroot_ownership() {
